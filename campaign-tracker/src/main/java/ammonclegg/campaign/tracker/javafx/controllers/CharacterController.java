55aedf1a534cdf82658012115500889dda5e2c89
Ammon Clegg <AmmonIsaiah.Clegg@familysearch.org>
2020-01-10 15:58:00 -0700

    Update character page with name and description
    I want to be able to see and update a character for the game. This page allows me to start writing up descriptions for the character. At some point I might allow linking a pdf or html version of the character sheet as well.

--- campaign-tracker/src/main/java/ammonclegg/campaign/tracker/javafx/controllers/CharacterController.java
+++ campaign-tracker/src/main/java/ammonclegg/campaign/tracker/javafx/controllers/CharacterController.java
@@ -2,7 +2,10 @@ package ammonclegg.campaign.tracker.javafx.controllers;
 
 import ammonclegg.campaign.tracker.models.GameCharacter;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.beans.PropertyChangeEvent;
@@ -15,15 +18,19 @@ import static ammonclegg.campaign.tracker.models.GameCharacter.NAME;
  */
 @Component
 public class CharacterController implements PropertyChangeListener {
+  private static final Logger LOGGER = LoggerFactory.getLogger(CharacterController.class);
 
   private GameCharacter character;
 
   @FXML
-  private Label characterName;
+  private TextField characterName;
+
+  @FXML
+  private TextArea characterDescription;
 
   @FXML
   public void initialize() {
-
+    characterDescription.textProperty().addListener((observable, oldValue, newValue)->character.setDescription(newValue));
   }
 
   public void setCharacter(GameCharacter character) {
@@ -37,10 +44,28 @@ public class CharacterController implements PropertyChangeListener {
 
   private void loadCharacter() {
     updateName();
+    updateDescriptionField();
   }
 
   private void updateName() {
     this.characterName.setText(character.getName());
+  }
+
+  private void updateDescriptionField() {
+    this.characterDescription.setText(character.getDescription());
+  }
+
+//  @FXML
+//  private void onCharacterNameEntry() {
+//    LOGGER.info("Updating character name");
+//    String text = characterName.getText();
+//    character.setName(text);
+//  }
+
+  @FXML
+  private void onUpdateDescription() {
+    String text = characterDescription.getText();
+    character.setDescription(text);
   }
 
   @Override
